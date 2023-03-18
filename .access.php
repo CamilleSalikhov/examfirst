509564f2446e42b09d86c9397e49df4c93849b98
Your Name <you@example.com>
2023-03-18 20:29:14 +0300

    first

---
+++ .access.php
@@ -0,0 +1,11 @@
+<?
+$PERM["news"]["G5"]="W";
+$PERM["news"]["G2"]="R";
+$PERM["index.php"]["G5"]="R";
+$PERM["search"]["5"]="W";
+$PERM["products"]["5"]="W";
+$PERM["services"]["5"]="W";
+$PERM["contacts"]["5"]="W";
+$PERM["company"]["5"]="W";
+$PERM["/"]["*"]="R";
+?>
---
+++ local/templates/exam1/components/bitrix/menu/myTopMenu/template.php
@@ -0,0 +1,141 @@
+ 
+
+
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<?if (!empty($arResult)):?>
+<ul id="horizontal-multilevel-menu">
+
+ 
+<?
+$previousLevel = 0;
+foreach($arResult as $arItem):?>
+
+	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
+		<?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
+	<?endif?>
+
+	<?if ($arItem["IS_PARENT"]):?>
+  
+		<?if ($arItem["DEPTH_LEVEL"] == 1):?>
+			<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
+				 
+				
+				
+				
+				<ul> <? $menu_text = $APPLICATION->GetDirProperty("menu_top_text", $arItem["LINK"]);
+				if($menu_text):
+				?>
+				  <div class ="menu-text"><?=$menu_text?></div>
+				
+				
+				<?endif;?>
+		<?else:?>
+		  
+			<li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>" class="parent"><?=$arItem["TEXT"]?></a>
+				
+				
+				
+				<ul> <? $menu_text = $APPLICATION->GetDirProperty("menu_top_text", $arItem["LINK"]);
+				if($menu_text):
+				?>
+				  <div class ="menu-text"><?=$menu_text?></div>
+				
+				
+				<?endif;?>
+		<?endif?>
+
+	<?else:?>
+
+		<?if ($arItem["PERMISSION"] > "D"):?>
+
+			<?if ($arItem["DEPTH_LEVEL"] == 1):?>
+				<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a></li>
+			<?else:?>
+				<li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+			<?endif?>
+
+		<?else:?>
+
+			 
+
+		<?endif?>
+
+	<?endif?>
+
+	<?$previousLevel = $arItem["DEPTH_LEVEL"];?>
+
+<?endforeach?>
+
+<?if ($previousLevel > 1)://close last item tags?>
+	<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
+<?endif?>
+
+</ul>
+				<a href="" class="btn-close"></a>
+<div class="menu-clear-left"></div>
+<?endif?>
+
+
+
+
+
+
+<!--
+<ul class="">
+                            <li class="main-page"><a href="">Главная</a>
+                            </li>
+                            <li>
+                                <a href="">Компания</a>
+                                <ul>
+                                    <li>
+                                        <a href="">Пункт 1</a>
+                                        <ul>
+                                            <li><a href="">Пункт 1</a>
+                                            </li>
+                                            <li><a href="">Пункт 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="">Пункт 2</a>
+                                    </li>
+                                    <li><a href="">Пункт 3</a>
+                                    </li>
+                                    <li><a href="">Пункт 4</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="">Новости</a>
+                            </li>
+                            <li>
+                                <a href="">Каталог</a>
+                                <ul>
+                                    <li>
+                                        <a href="">Пункт 1</a>
+                                        <ul>
+                                            <li><a href="">Пункт 1</a>
+                                            </li>
+                                            <li><a href="">Пункт 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="">Пункт 2</a>
+                                    </li>
+                                    <li><a href="">Пункт 3</a>
+                                    </li>
+                                    <li><a href="">Пункт 4</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="">Фотогалерея</a>
+                            </li>
+                            <li><a href="">Партнерам</a>
+                            </li>
+                            <li><a href="">Контакты</a>
+                            </li>
+                        </ul>-->
+
+                       <!-- <a href="" class="btn-close"></a> это уже добавил-->
+
+ 
+
